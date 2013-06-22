49f017640890ff9e15177045cfd9ca6928489661
Dominic Cleal <dcleal@redhat.com>
2013-06-22 23:20:04 +0100

    Configure installer answers file with custom repos

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -9,13 +9,25 @@ RSpec.configure do |c|
 
   c.before :suite do
     unless ENV['FOREMAN_SKIP_INSTALL'] && ENV['FOREMAN_SKIP_INSTALL'] == '1'
-      opts = {}
-      opts[:repo] = ENV['FOREMAN_REPO'] if ENV['FOREMAN_REPO']
-      opts[:custom_repo] = ENV['FOREMAN_CUSTOM_REPO'] if ENV['FOREMAN_CUSTOM_REPO']
-      opts[:installer_source] = ENV['FOREMAN_INSTALLER_SOURCE'] if ENV['FOREMAN_INSTALLER_SOURCE']
+      installer = {}
+      answers = {:foreman => {}, :foreman_proxy => {}}
 
-      foreman_installer_install opts
-      foreman_install
+      if ENV['FOREMAN_REPO']
+        installer[:repo] = ENV['FOREMAN_REPO']
+        answers[:foreman][:repo] = ENV['FOREMAN_REPO']
+        answers[:foreman_proxy][:repo] = ENV['FOREMAN_REPO']
+      end
+
+      if ENV['FOREMAN_CUSTOM_REPO']
+        installer[:custom_repo] = ENV['FOREMAN_CUSTOM_REPO']
+        answers[:foreman][:custom_repo] = true
+        answers[:foreman_proxy][:custom_repo] = true
+      end
+
+      installer[:installer_source] = ENV['FOREMAN_INSTALLER_SOURCE'] if ENV['FOREMAN_INSTALLER_SOURCE']
+
+      foreman_installer_install installer
+      foreman_install :answers => answers
     end
   end
 end
